85eef385ed927962d321b03dd4c7c551a9f707b3
rgrp <rufus.pollock@okfn.org>
2011-11-08 16:12:56 +0000

    [demo][s]: allow loading of demo data viewer from a webstore url.

--- demo/js/app.js
+++ demo/js/app.js
@@ -262,6 +262,8 @@ app.sammy = $.sammy(function () {
 });
 
 $(function() {
+  var demoUrl = 'http://webstore.test.ckan.org/rufuspollock/demo/data';
+  $('.dataexplorer-tableview-nav form input[name="source"]').val(demoUrl);
   var datasetId = 'test-dataset';
   var metadata = {
     title: 'My Test Dataset'
@@ -280,7 +282,7 @@ $(function() {
     ]
   };
   // this is all rather artificial here but would make more sense with more complex backend
-  backend = new recline.BackendMemory();
+  var backend = new recline.BackendMemory();
   backend.addDataset({
     metadata: metadata,
     data: indata
@@ -293,6 +295,20 @@ $(function() {
       url: "awesome.com/webstore.json"
     })
     
+    $('.container').append(dataTable.el)
+  });
+  $('.dataexplorer-tableview-nav form').submit(function(e) {
+    e.preventDefault();
+    var $form = $(e.target);
+    var source = $form.find('input[name="source"]').val();
+    var backend = new recline.BackendWebstore({
+      url: source
+    });
+    recline.setBackend(backend);
+    var dataset = backend.getDataset();
+    var dataTable = new recline.DataTable({
+      model: dataset
+    })
     $('.container').append(dataTable.el)
   });
 })
