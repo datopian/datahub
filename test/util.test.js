ed3d20b20f0561eb00d9e74b7e808c8b362adc66
Rufus Pollock <rufus.pollock@okfn.org>
2012-02-17 21:47:51 +0000

    [#42,flot-graph][s]: reinstate configuration of graph from routing info lost in penultimate commit a9cfd4412c8a4b9645e100997a4d1898620b0290.
    * view.js: bunch of utility functions for parsing and composing query strings for hash routing (plus tests).

---
+++ test/util.test.js
@@ -0,0 +1,22 @@
+(function ($) {
+module("Util");
+
+test('parseHashUrl', function () {
+  var out = recline.View.parseHashUrl('graph?x=y');
+  equal(out.path, 'graph');
+  equal(out.query, '?x=y');
+  var out = recline.View.parseHashUrl('graph');
+  equal(out.path, 'graph');
+  equal(out.query, '');
+});
+
+test('composeQueryString', function () {
+  var params = {
+    x: 'y',
+    a: 'b'
+  };
+  var out = recline.View.composeQueryString(params);
+  equal(out, '?x="y"&a="b"');
+});
+
+})(this.jQuery);
