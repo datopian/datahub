8a053d2b626e770ced3ea4ac9a9042589ac9922b
Rufus Pollock <rufus.pollock@okfn.org>
2012-02-17 22:36:22 +0000

    [whitespace][xs]: whitespace.

--- src/model.js
+++ src/model.js
@@ -3,85 +3,86 @@ this.recline = this.recline || {};
 this.recline.Model = this.recline.Model || {};
 
 (function($, my) {
-  // ## A Dataset model
-  //
-  // Other than standard list of Backbone methods it has two important attributes:
-  //
-  // * currentDocuments: a DocumentList containing the Documents we have currently loaded for viewing (you update currentDocuments by calling getRows)
-  // * docCount: total number of documents in this dataset (obtained on a fetch for this Dataset)
-  my.Dataset = Backbone.Model.extend({
-    __type__: 'Dataset',
-    initialize: function(model, backend) {
-      this.backend = backend;
-      if (backend && backend.constructor == String) {
-        this.backend = my.backends[backend];
-      }
-      this.currentDocuments = new my.DocumentList();
-      this.docCount = null;
-      this.defaultQuery = {
-        size: 100
-        , offset: 0
-      };
-      // this.queryState = {};
-    },
 
-    // ### getDocuments
-    //
-    // AJAX method with promise API to get rows (documents) from the backend.
-    //
-    // Resulting DocumentList are used to reset this.currentDocuments and are
-    // also returned.
-    //
-    // :param numRows: passed onto backend getDocuments.
-    // :param start: passed onto backend getDocuments.
-    //
-    // this does not fit very well with Backbone setup. Backbone really expects you to know the ids of objects your are fetching (which you do in classic RESTful ajax-y world). But this paradigm does not fill well with data set up we have here.
-    // This also illustrates the limitations of separating the Dataset and the Backend
-    query: function(queryObj) {
-      var self = this;
-      this.queryState = queryObj || this.defaultQuery;
-      this.queryState = _.extend({size: 100, offset: 0}, this.queryState);
-      var dfd = $.Deferred();
-      this.backend.query(this, this.queryState).done(function(rows) {
-        var docs = _.map(rows, function(row) {
-          var _doc = new my.Document(row);
-          _doc.backend = self.backend;
-          _doc.dataset = self;
-          return _doc;
-        });
-        self.currentDocuments.reset(docs);
-        dfd.resolve(self.currentDocuments);
-      })
-      .fail(function(arguments) {
-        dfd.reject(arguments);
-      });
-      return dfd.promise();
-    },
-
-    toTemplateJSON: function() {
-      var data = this.toJSON();
-      data.docCount = this.docCount;
-      return data;
+// ## A Dataset model
+//
+// Other than standard list of Backbone methods it has two important attributes:
+//
+// * currentDocuments: a DocumentList containing the Documents we have currently loaded for viewing (you update currentDocuments by calling getRows)
+// * docCount: total number of documents in this dataset (obtained on a fetch for this Dataset)
+my.Dataset = Backbone.Model.extend({
+  __type__: 'Dataset',
+  initialize: function(model, backend) {
+    this.backend = backend;
+    if (backend && backend.constructor == String) {
+      this.backend = my.backends[backend];
     }
-  });
+    this.currentDocuments = new my.DocumentList();
+    this.docCount = null;
+    this.defaultQuery = {
+      size: 100
+      , offset: 0
+    };
+    // this.queryState = {};
+  },
 
-  // ## A Document (aka Row)
-  // 
-  // A single entry or row in the dataset
-  my.Document = Backbone.Model.extend({
-    __type__: 'Document'
-  });
-
-  // ## A Backbone collection of Documents
-  my.DocumentList = Backbone.Collection.extend({
-    __type__: 'DocumentList',
-    model: my.Document
-  });
-
-  // ## Backend registry
+  // ### getDocuments
   //
-  // Backends will register themselves by id into this registry
-  my.backends = {};
+  // AJAX method with promise API to get rows (documents) from the backend.
+  //
+  // Resulting DocumentList are used to reset this.currentDocuments and are
+  // also returned.
+  //
+  // :param numRows: passed onto backend getDocuments.
+  // :param start: passed onto backend getDocuments.
+  //
+  // this does not fit very well with Backbone setup. Backbone really expects you to know the ids of objects your are fetching (which you do in classic RESTful ajax-y world). But this paradigm does not fill well with data set up we have here.
+  // This also illustrates the limitations of separating the Dataset and the Backend
+  query: function(queryObj) {
+    var self = this;
+    this.queryState = queryObj || this.defaultQuery;
+    this.queryState = _.extend({size: 100, offset: 0}, this.queryState);
+    var dfd = $.Deferred();
+    this.backend.query(this, this.queryState).done(function(rows) {
+      var docs = _.map(rows, function(row) {
+        var _doc = new my.Document(row);
+        _doc.backend = self.backend;
+        _doc.dataset = self;
+        return _doc;
+      });
+      self.currentDocuments.reset(docs);
+      dfd.resolve(self.currentDocuments);
+    })
+    .fail(function(arguments) {
+      dfd.reject(arguments);
+    });
+    return dfd.promise();
+  },
+
+  toTemplateJSON: function() {
+    var data = this.toJSON();
+    data.docCount = this.docCount;
+    return data;
+  }
+});
+
+// ## A Document (aka Row)
+// 
+// A single entry or row in the dataset
+my.Document = Backbone.Model.extend({
+  __type__: 'Document'
+});
+
+// ## A Backbone collection of Documents
+my.DocumentList = Backbone.Collection.extend({
+  __type__: 'DocumentList',
+  model: my.Document
+});
+
+// ## Backend registry
+//
+// Backends will register themselves by id into this registry
+my.backends = {};
 
 }(jQuery, this.recline.Model));
 
