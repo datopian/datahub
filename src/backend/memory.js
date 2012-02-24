f0d8265c879f456b88161f595f62d6829d1ffb78
Rufus Pollock <rufus.pollock@okfn.org>
2012-02-24 14:14:20 +0000

    [backend,refactor][xs]: remove stuff from memory backend that should have gone in last commit.

--- src/backend/memory.js
+++ src/backend/memory.js
@@ -9,38 +9,6 @@ this.recline = this.recline || {};
 this.recline.Backend = this.recline.Backend || {};
 
 (function($, my) {
-  // ## Backbone.sync
-  //
-  // Override Backbone.sync to hand off to sync function in relevant backend
-  Backbone.sync = function(method, model, options) {
-    return model.backend.sync(method, model, options);
-  }
-
-  // ## wrapInTimeout
-  // 
-  // Crude way to catch backend errors
-  // Many of backends use JSONP and so will not get error messages and this is
-  // a crude way to catch those errors.
-  function wrapInTimeout(ourFunction) {
-    var dfd = $.Deferred();
-    var timeout = 5000;
-    var timer = setTimeout(function() {
-      dfd.reject({
-        message: 'Request Error: Backend did not respond after ' + (timeout / 1000) + ' seconds'
-      });
-    }, timeout);
-    ourFunction.done(function(arguments) {
-        clearTimeout(timer);
-        dfd.resolve(arguments);
-      })
-      .fail(function(arguments) {
-        clearTimeout(timer);
-        dfd.reject(arguments);
-      })
-      ;
-    return dfd.promise();
-  }
-
   // ## BackendMemory - uses in-memory data
   //
   // This is very artificial and is really only designed for testing
