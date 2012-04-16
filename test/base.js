53e099beda3d41ea21dc906f3cd5a3beff0dc582
Rufus Pollock <rufus.pollock@okfn.org>
2012-04-16 15:15:59 +0100

    [test][xs]: make fixtures dataset same as for demo.

--- test/base.js
+++ test/base.js
@@ -1,13 +1,21 @@
 var Fixture = {
   getDataset: function() {
-    var fields = [{id: 'x'}, {id: 'y'}, {id: 'z'}, {id: 'country'}, {id: 'label'},{id: 'lat'},{id: 'lon'}];
+    var fields = [
+      {id: 'x'},
+      {id: 'y'},
+      {id: 'z'},
+      {id: 'country'},
+      {id: 'label'},
+      {id: 'lat'},
+      {id: 'lon'}
+    ];
     var documents = [
-      {id: 0, x: 1, y: 2, z: 3, country: 'DE', lat:52.56, lon:13.40}
-      , {id: 1, x: 2, y: 4, z: 6, country: 'UK', lat:54.97, lon:-1.60}
-      , {id: 2, x: 3, y: 6, z: 9, country: 'US', lat:40.00, lon:-75.5}
-      , {id: 3, x: 4, y: 8, z: 12, country: 'UK', lat:57.27, lon:-6.20}
-      , {id: 4, x: 5, y: 10, z: 15, country: 'UK', lat:51.58, lon:0}
-      , {id: 5, x: 6, y: 12, z: 18, country: 'DE', lat:51.04, lon:7.9}
+      {id: 0, x: 1, y: 2, z: 3, country: 'DE', label: 'first', lat:52.56, lon:13.40},
+      {id: 1, x: 2, y: 4, z: 6, country: 'UK', label: 'second', lat:54.97, lon:-1.60},
+      {id: 2, x: 3, y: 6, z: 9, country: 'US', label: 'third', lat:40.00, lon:-75.5},
+      {id: 3, x: 4, y: 8, z: 12, country: 'UK', label: 'fourth', lat:57.27, lon:-6.20},
+      {id: 4, x: 5, y: 10, z: 15, country: 'UK', label: 'fifth', lat:51.58, lon:0},
+      {id: 5, x: 6, y: 12, z: 18, country: 'DE', label: 'sixth', lat:51.04, lon:7.9}
     ];
     var dataset = recline.Backend.createDataset(documents, fields);
     return dataset;
