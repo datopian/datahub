7e67414187773ae0c940f6d943a05f9a3fb2f8b1
amercader <amercadero@gmail.com>
2012-05-25 18:48:27 +0100

    [xs] Remove trailing comma

--- test/base.js
+++ test/base.js
@@ -17,7 +17,7 @@ var Fixture = {
       {id: 2, date: '2011-03-03', x: 3, y: 6, z: 9, country: 'US', label: 'third', lat:40.00, lon:-75.5},
       {id: 3, date: '2011-04-04', x: 4, y: 8, z: 6, country: 'UK', label: 'fourth', lat:57.27, lon:-6.20},
       {id: 4, date: '2011-05-04', x: 5, y: 10, z: 15, country: 'UK', label: 'fifth', lat:51.58, lon:0},
-      {id: 5, date: '2011-06-02', x: 6, y: 12, z: 18, country: 'DE', label: 'sixth', lat:51.04, lon:7.9},
+      {id: 5, date: '2011-06-02', x: 6, y: 12, z: 18, country: 'DE', label: 'sixth', lat:51.04, lon:7.9}
     ];
     var dataset = recline.Backend.createDataset(documents, fields);
     return dataset;
