datopian/datahub

commit 882d31eca577fcb35be2c23995af29f1d6e441ac

[dist][s]: move recline.js to dist/recline.js.

make
@@ -5,7 +5,8 @@ import os
 
 def cat():
     print("** Combining js files")
-    cmd = 'cat src/*.js src/backend/*.js > recline.js'
+    cmd = 'cat src/*.js src/backend/*.js > dist/recline.js'
+    cmd = 'cat css/*.css > dist/recline.css'
     os.system(cmd)
 
 def docs():

_includes/recline-deps.html
@@ -29,5 +29,5 @@
   <script type="text/javascript" src="src/backend/base.js"></script>
   <script type="text/javascript" src="src/backend/memory.js"></script>
   -->
-  <script type="text/javascript" src="recline.js"></script>
+  <script type="text/javascript" src="dist/recline.js"></script>
 

download.markdown
@@ -17,7 +17,7 @@ unit tests, files for debugging and a build system. The production files
 
 Just want the all-in-one file containing all of Recline library in a single file? Here it is:
 
-<p><a href="recline.js" class="btn">recline.js all-in-one (master)</a></p>
+<p><a href="dist/recline.js" class="btn">recline.js all-in-one (master)</a></p>
 
 [View Changelog](https://github.com/okfn/recline#changelog)
 

example-backends.markdown
@@ -76,7 +76,7 @@ This is as per the [quickstart](example-quickstart.html) but the set of files is
 <script type="text/javascript" src="src/backend/gdocs.js"></script>
 
 <!-- Or you can just include all of recline. -->
-<script type="text/javascript" src="recline.js"></script>
+<script type="text/javascript" src="dist/recline.js"></script>
 {% endhighlight %}
 
 

example-quickstart.markdown
@@ -37,7 +37,7 @@ Before writing any code with Recline, you need to do the following preparation s
 <script type="text/javascript" src="src/backend/memory.js"></script>
 <script type="text/javascript" src="src/view-grid.js"></script>
 -->
-<script type="text/javascript" src="recline.js"></script>{% endhighlight %}
+<script type="text/javascript" src="dist/recline.js"></script>{% endhighlight %}
 
 4. Create a div to hold the Recline view(s):
     {% highlight html %}

commit 3fbad6bace12e1c14b57cd53836d848e166a1135

[make,bugfix][xs]: concat js files as well as css (problem introduced in 882d31eca577fcb35be2c23995af29f1d6e441ac).

make
@@ -6,6 +6,8 @@ import os
 def cat():
     print("** Combining js files")
     cmd = 'cat src/*.js src/backend/*.js > dist/recline.js'
+    os.system(cmd)
+    print("** Combining css files")
     cmd = 'cat css/*.css > dist/recline.css'
     os.system(cmd)
 