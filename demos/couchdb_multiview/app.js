40513310ceda37516101cf8dcb1785a6037d65ca
Bietz <reason@BackDoors-MacBook-Air.local>
2012-08-06 14:53:34 -0700

    fixed couchdb backend, updated backbone and underscore, added couchdb multiview example in demos

---
+++ demos/couchdb_multiview/app.js
@@ -0,0 +1,90 @@
+jQuery(function($) {
+  window.dataExplorer = null;
+  window.explorerDiv = $('.data-explorer-here');
+
+  // This is some fancy stuff to allow configuring the multiview from
+  // parameters in the query string
+  //
+  // For more on state see the view documentation.
+  var state = recline.View.parseQueryString(decodeURIComponent(window.location.search));
+  if (state) {
+    _.each(state, function(value, key) {
+      try {
+        value = JSON.parse(value);
+      } catch(e) {}
+      state[key] = value;
+    });
+  } else {
+    state.url = 'demo';
+  }
+
+//Add your couchdb here!!!
+//"couchdb" is what a choose for my reverse proxy.  look up apache reverse or nginx reverse proxy. you need to set this up before you can connect your db
+var dataset = new recline.Model.Dataset({
+        db_url: '/couchdb/yourcouchdb',
+        view_url: '/couchdb/yourcouchdb/_design/yourdesigndoc/_view/yourview',
+        backend: 'couchdb',
+        query_options: {
+                'key': '_id'
+        }
+});
+
+
+dataset.fetch().done(function(dataset) {
+    console.log('records: ' + dataset.records);
+});
+
+  createExplorer(dataset, state);
+});
+
+// make Explorer creation / initialization in a function so we can call it
+// again and again
+var createExplorer = function(dataset, state) {
+  // remove existing data explorer view
+  var reload = false;
+  if (window.dataExplorer) {
+    window.dataExplorer.remove();
+    reload = true;
+  }
+  window.dataExplorer = null;
+  var $el = $('<div />');
+  $el.appendTo(window.explorerDiv);
+
+  var views = [
+    {
+      id: 'grid',
+      label: 'Grid',
+      view: new recline.View.SlickGrid({
+        model: dataset
+      }),
+    },
+    {
+      id: 'graph',
+      label: 'Graph',
+      view: new recline.View.Graph({
+        model: dataset
+      }),
+    },
+    {
+      id: 'map',
+      label: 'Map',
+      view: new recline.View.Map({
+        model: dataset
+      }),
+    },
+    {
+      id: 'transform',
+      label: 'Transform',
+      view: new recline.View.Transform({
+        model: dataset
+      })
+    }
+  ];
+
+  window.dataExplorer = new recline.View.MultiView({
+    model: dataset,
+    el: $el,
+    state: state,
+    views: views
+  });
+}
