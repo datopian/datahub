518634acaa5ad2cc8a3e068c49b3e1380cf54acd
Aaron Couch <acinternets@gmail.com>
2012-09-03 00:30:08 -0500

    Update src/backend.csv.js
    Adding $ parameter to function definition for compatibility with noConflict().

--- src/backend.csv.js
+++ src/backend.csv.js
@@ -2,7 +2,7 @@ this.recline = this.recline || {};
 this.recline.Backend = this.recline.Backend || {};
 this.recline.Backend.CSV = this.recline.Backend.CSV || {};
 
-(function(my) {
+(function($, my) {
   // ## fetch
   //
   // 3 options
@@ -242,4 +242,4 @@ this.recline.Backend.CSV = this.recline.Backend.CSV || {};
   }
 
 
-}(this.recline.Backend.CSV));
+}(jQuery, this.recline.Backend.CSV));
