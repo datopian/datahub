b2f90d5f6a09c4ce63b6fec2f8b30eb00183764e
aliounedia <dia.aliounes@gmail.com>
2014-09-25 11:28:09 +0200

    Fix bug in backends ducumentation #449

--- _includes/example-backends-online-csv.js
+++ _includes/example-backends-online-csv.js
@@ -1,7 +1,7 @@
 // Create the dataset in the usual way
 // Note the additional options you can specify for parsing the CSV file
 var dataset = new recline.Model.Dataset({
-  url: '{{page.root}}/demos/data/sample.csv',
+  url: '{{page.root}}demos/data/sample.csv',
   backend: 'csv',
   // delimiter: ',',
   // quotechar: '"',
@@ -11,6 +11,7 @@ var dataset = new recline.Model.Dataset({
 // remember this is async so if you want to do something you need to call it in done method e.g.
 // dataset.fetch.done(function(dataset) { console.log(dataset.recordCount)});
 dataset.fetch();
+
 // show the data for illustrations sake
 var grid = new recline.View.SlickGrid({
   model: dataset,
