56945ed3f2b36a819a2d2d2eeeec41db2184fccf
georgiamoon <georgia.bullen@gmail.com>
2015-04-05 22:08:49 -0400

    fixes to the map customize tutorial
    removed the record line, added some more customization examples

--- _includes/tutorial-maps-customize.js
+++ _includes/tutorial-maps-customize.js
@@ -8,9 +8,17 @@ view.geoJsonLayerOptions.pointToLayer = function(feature, latlng) {
   // Look up Record so we can use it to customize size of marker
   // note that 'this' is specially bound for us to parent view + that feature
   // stores record cid
-  var record = this.model.records.getByCid(feature.properties.cid);
-  var marker = new L.CircleMarker(latlng, { radius: record.get('x') * 3 });
+  
+  var marker = new L.CircleMarker(latlng, { 
+  	radius: 10,
+    fillColor: "#333",
+    color: "#000",
+    weight: 0.5,
+    opacity: 0.5,
+    fillOpacity: 0.5
+  });
   marker.bindPopup(feature.properties.popupContent);
+  this.markers.addLayer(marker);
   return marker;
 }
 view.render();
