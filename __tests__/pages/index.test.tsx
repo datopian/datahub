c9d357f6f521d5c34230a9ba8d8f44f35f04bad8
anuveyatsu <anuar.ustayev@gmail.com>
2020-06-12 16:45:26 +0600

    [tests][xs]: updated test name and snapshot on home page.

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import renderer from 'react-test-renderer'
 import Index from '../../pages/index'
 
-test('renders snapshot homepage', ()  => {
+test('📸 of Home page', ()  => {
   const tree = renderer.create(<Index />).toJSON()
   expect(tree).toMatchSnapshot()
 })
