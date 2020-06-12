8abf32c4029fc320c5f0d398828b9fc44a7b8730
anuveyatsu <anuar.ustayev@gmail.com>
2020-06-12 19:35:37 +0600

    [bug fix][xs]: don't fail if dataset has no owner org.

--- components/dataset/Org.tsx
+++ components/dataset/Org.tsx
@@ -3,10 +3,15 @@ import Link from 'next/link'
 export default function Org({ org }) {
   return (
     <>
-      <img src={org.image_url} className="h-5 w-5 mr-2 inline-block" />
-      <Link href={`/@${org.name}`}>
-        <a className="font-semibold text-primary underline">{ org.title || org.name }</a>
-      </Link>
+    {org
+      ? <>
+          <img src={org.image_url || 'https://datahub.io/static/img/datahub-cube-edited.svg'} className="h-5 w-5 mr-2 inline-block" />
+          <Link href={`/@${org.name}`}>
+            <a className="font-semibold text-primary underline">{ org.title || org.name }</a>
+          </Link>
+        </>
+      : ''
+    }
     </>
   )
 }
