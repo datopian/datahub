fa154d45de8273af730eff691203758842147a52
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-11-24 15:35:42 -0300

    [todo] [s]: add refactor comment

--- packages/create-portal-app/helpers/copy.js
+++ packages/create-portal-app/helpers/copy.js
@@ -4,6 +4,7 @@ const spawn  = require('cross-spawn');
 
 module.exports = function copy(root,destination){
   const dest_path = [process.cwd(), destination].join(path.sep)
+  //TODO Move this method to validate in another function to throw a error and prompt another name
   if(fs.existsSync(dest_path)){
     if(fs.readdirSync(dest_path).length > 0) return Promise.reject(`directory ${dest_path} exist and not empty`);
   }
