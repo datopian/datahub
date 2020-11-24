d42a857deacc4ec428e374c9eca4e260d8f6cd4d
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-11-24 12:20:49 -0300

    [copy] [s]: update copy method

--- packages/create-portal-app/helpers/copy.js
+++ packages/create-portal-app/helpers/copy.js
@@ -1,16 +1,11 @@
-
-const cpy = require("cpy")
 const path = require("path");
 const fs = require('fs');
+const spawn  = require('cross-spawn');
 
 module.exports = function copy(root,destination){
   const dest_path = [process.cwd(), destination].join(path.sep)
   if(fs.existsSync(dest_path)){
     if(fs.readdirSync(dest_path).length > 0) return Promise.reject(`directory ${dest_path} exist and not empty`);
   }
-  return cpy(root, dest_path);
+  return spawn.sync('cp', ['-r', root, dest_path]);
 }
-
-// (async ()=> {
-//   await copy([process.cwd(),"templates"].join(path.sep), "newme");
-// })().catch(e=> console.log(chalk.red(e)));
