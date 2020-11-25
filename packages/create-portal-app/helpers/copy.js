fe25a41009404982329e3171d22d99e0b26cb442
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-11-25 11:44:46 -0300

    [refactor] [s]: move path parser to a new function

--- packages/create-portal-app/helpers/copy.js
+++ packages/create-portal-app/helpers/copy.js
@@ -2,11 +2,15 @@ const path = require("path");
 const fs = require('fs');
 const spawn  = require('cross-spawn');
 
+function _parserPath(path){
+  return [process.cwd(), destination].join(path.sep)
+}
 module.exports = function copy(root,destination){
-  const dest_path = [process.cwd(), destination].join(path.sep)
+  const dest_path = _parserPath(destination)
   //TODO Move this method to validate in another function to throw a error and prompt another name
   if(fs.existsSync(dest_path)){
     if(fs.readdirSync(dest_path).length > 0) return Promise.reject(`directory ${dest_path} exist and not empty`);
   }
   return spawn.sync('cp', ['-r', root, dest_path]);
 }
+
