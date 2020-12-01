96e0659d1a5f2dc70d81d5c3f059053af7c39afd
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-12-01 12:09:47 -0300

    [demo] [s]: remove unused file

--- packages/create-portal-app/demo_index.js
+++
@@ -1,9 +0,0 @@
-const copy = require("./helpers/copy");
-const install = require("./helpers/install");
-const path = require('path');
-
-(async ()=>{
-  const root = [__dirname, "templates"].join(path.sep);
-  await copy(root,"Datahub")
-  await install("Datahub", true);
-})().catch(e=> console.log(e));
