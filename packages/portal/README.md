5e8709ca89a1cf06172fdf8d8739b59319abfdbf
Rising Odegua <risingodegua@gmail.com>
2021-05-03 09:30:15 +0100

    [Src][s]: Move src to root folder

--- packages/portal/README.md
+++
@@ -1,1 +0,0 @@
-Portaljs component library
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { PlotlyChart } from './components/views/Chart'
 import KeyInfo from './components/page/KeyInfo'
 import ResourceInfo from './components/page/ResourceInfo'
 import ReadMe from './components/page/Readme'
+import Nav from './components/ui/Home/Nav'
+import Recent from './components/ui/Home/Recent'
 
-
-export { Table, PlotlyChart, KeyInfo, ResourceInfo, ReadMe }
+export { Table, PlotlyChart, KeyInfo, ResourceInfo, ReadMe, Nav, Recent }
