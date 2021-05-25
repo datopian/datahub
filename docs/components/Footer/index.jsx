cbab921e8789dfd353bbc7f83f2ce8a9b1c3e514
Rufus Pollock <rufus.pollock@datopian.com>
2021-05-25 19:00:41 +0200

    [docs,refactor][xs]: move to 2 space indenting in jsx components.

--- docs/components/Footer/index.jsx
+++ docs/components/Footer/index.jsx
@@ -1,16 +1,16 @@
 import styles from '../../styles/Home.module.css'
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <a
-                href="https://www.datopian.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Built by{' '}
-                <img src="/datopian-logo.png" alt="Datopian Logo" className={styles.logo} />
-            </a>
-        </footer>
-    )
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://www.datopian.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Built by{' '}
+        <img src="/datopian-logo.png" alt="Datopian Logo" className={styles.logo} />
+      </a>
+    </footer>
+  )
 }
