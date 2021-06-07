072762ab2308b04355418432347231e591d0754d
Rufus Pollock <rufus.pollock@datopian.com>
2021-06-07 20:28:55 +0200

    [home][xs]: remove module.css as we moved to tailwind in last commit 6e81d5d7078238feec76e2e6357d92a4149a02b6.

--- docs/components/Footer/index.jsx
+++ docs/components/Footer/index.jsx
@@ -1,5 +1,3 @@
-import styles from '../../styles/Home.module.css'
-
 export default function Footer() {
   return (
     <footer className="flex items-center justify-center w-full h-24 border-t">
--- docs/pages/index.js
+++ docs/pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
