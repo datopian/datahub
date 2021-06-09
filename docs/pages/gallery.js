23b7bd549780ca922af4da47dbee9e39b13f84a5
Rising Odegua <risingodegua@gmail.com>
2021-06-09 09:29:58 +0100

    [Docs][s]: Fix style issue in docs

--- docs/pages/gallery.js
+++ docs/pages/gallery.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
-import styles from '../styles/Home.module.css'
 
 export default function Gallery() {
 
@@ -12,8 +11,8 @@ export default function Gallery() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
-      <div className={styles.container}>
-        <main className={styles.main}>
+      <div >
+        <main >
 
         </main>
         <Footer />
