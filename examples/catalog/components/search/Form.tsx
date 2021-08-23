40b3f52564cad867496c323ac70d5f483ea1e1fe
anuveyatsu <anuar.ustayev@gmail.com>
2021-08-23 11:36:15 +0600

    [catalog/setup/css][s]: small bug fix re missing argument; remove tailwind purge option as already purging via postcss.

--- examples/catalog/components/search/Form.tsx
+++ examples/catalog/components/search/Form.tsx
@@ -29,7 +29,7 @@ const SearchForm: React.FC = () => {
         className="inline-block w-1/2 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
       />
       <button
-        onClick={() => handleSubmit()}
+        onClick={() => handleSubmit(false)}
         type="button"
         className="inline-block text-sm px-4 py-3 mx-3 leading-none border rounded text-white bg-black border-black lg:mt-0"
       >
--- examples/catalog/tailwind.config.js
+++ examples/catalog/tailwind.config.js
@@ -2,16 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
-  purge: [
-    './pages/**/*.js',
-    './pages/**/*.ts',
-    './pages/**/*.jsx',
-    './pages/**/*.tsx',
-    './components/**/*.js',
-    './components/**/*.ts',
-    './components/**/*.jsx',
-    './components/**/*.tsx',
-  ],
+  purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
     container: {
