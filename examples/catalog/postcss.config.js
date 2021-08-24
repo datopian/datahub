6bc36f9e3b4775aac5b0f533751624cfdb016b22
anuveyatsu <anuar.ustayev@gmail.com>
2021-08-24 11:54:01 +0600

    [catalog/css setup][xs]: removed purging css as it is not working as expected. Removed theme configs in tailwind config. Note that CSS asset in production is now 298kb which is a lot comparing to 4kb previously.

--- examples/catalog/postcss.config.js
+++ examples/catalog/postcss.config.js
@@ -1,17 +1,8 @@
-const purgecss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    content: ['./components/**/*.tsx', './pages/**/*.tsx'],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
-
 module.exports = {
   plugins: [
     'postcss-preset-env',
     'postcss-import',
     'tailwindcss',
     'autoprefixer',
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 };
--- examples/catalog/tailwind.config.js
+++ examples/catalog/tailwind.config.js
@@ -4,18 +4,8 @@ module.exports = {
   mode: 'jit',
   purge: false,
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      fontFamily: {
-        mono: ['Inconsolata', ...defaultTheme.fontFamily.mono],
-      },
-    },
-  },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms')],
 };
