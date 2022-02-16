ef2382cb6e55b9c90670c43f04fca5888c3f658b
Rising Odegua <risingodegua@gmail.com>
2022-02-16 20:22:40 +0100

    Install MUI icon library

--- rollup.config.js
+++ rollup.config.js
@@ -3,50 +3,18 @@ import external from 'rollup-plugin-peer-deps-external';
 import del from 'rollup-plugin-delete';
 import pkg from './package.json';
 
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-
-const EXTENSIONS = ['.ts', '.tsx', '.js', '.jsx', '.json'];
-
-const commonPlugins = () => [
-    external({
-        includeDependencies: true,
-    }),
-    babel({
-        babelrc: false,
-        presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
-        extensions: EXTENSIONS,
-        exclude: 'node_modules/**',
-    }),
-    commonjs({
-        include: /node_modules/,
-    }),
-    resolve({
-        extensions: EXTENSIONS,
-        preferBuiltins: false,
-    }),
-    del({ targets: ['dist/*'] }),
-];
-
 export default {
     input: pkg.source,
     output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-            globals: {
-                react: 'React',
-                'react-dom': 'ReactDOM',
-            }
-        },
-        {
-            file: pkg.module,
-            format: 'esm',
-            globals: {
-                react: 'React',
-                'react-dom': 'ReactDOM',
-            },
-        }
+        { file: pkg.main, format: 'cjs' },
+        { file: pkg.module, format: 'esm' }
     ],
-    plugins: commonPlugins(),
+    plugins: [
+        external(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        del({ targets: ['dist/*'] }),
+    ],
+    external: Object.keys(pkg.peerDependencies || {}),
 };
