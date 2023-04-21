110360ccae36371cdb54ffd21a72857175576402
deme <joaommdemenech@gmail.com>
2023-04-21 14:55:33 -0300

    Revert "[website,#778][xs]: learn page removed"
    This reverts commit 65e2a8be4c904785080c33a74d377ce8983aeaf3.

--- site/content/config.js
+++ site/content/config.js
@@ -18,6 +18,7 @@ const config = {
   navLinks: [
     { name: "Docs", href: "/docs" },
     { name: "Components", href: "/docs/components" },
+    { name: "Learn", href: "/learn" },
     { name: "Blog", href: "/blog" },
     // { name: "Gallery", href: "/gallery" },
     // { name: "Data Literate", href: "/data-literate" },
