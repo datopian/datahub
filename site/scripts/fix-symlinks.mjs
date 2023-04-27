ce8a92aa291b8715fe3d3a8f2499492fbb96c314
deme <joaommdemenech@gmail.com>
2023-04-27 08:56:35 -0300

    [assets][xs]: rebuilds symlinks on Vercel

---
+++ site/scripts/fix-symlinks.mjs
@@ -0,0 +1,16 @@
+// Script executed before builds
+
+import fs from "fs";
+
+//  If Vercel environment is detected
+if (process.env.VERCEL_ENV) {
+  console.log(
+    "[scripts/fix-symlinks.mjs] Vercel environment detected. Fixing symlinks..."
+  );
+
+  const pathToAssetsLn = "./public/assets";
+
+  fs.unlinkSync(pathToAssetsLn);
+
+  fs.symlinkSync("../../public/assets", pathToAssetsLn);
+}
