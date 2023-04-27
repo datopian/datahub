0317ae1815bf2ee7e15ff5babaa7af41a3c9f0e3
deme <joaommdemenech@gmail.com>
2023-04-27 09:22:21 -0300

    [assets, symlink][xs]: add log to see folder structure on Vercel

--- site/scripts/fix-symlinks.mjs
+++ site/scripts/fix-symlinks.mjs
@@ -8,9 +8,10 @@ if (process.env.VERCEL_ENV) {
     "[scripts/fix-symlinks.mjs] Vercel environment detected. Fixing symlinks..."
   );
 
-  const pathToAssetsLn = "../public/assets";
+  console.log(fs.readdirSync(process.cwd()))
 
-  console.log(process.cwd());
+  const pathToAssetsLn = "./public/assets";
+
   fs.unlinkSync(pathToAssetsLn);
 
   fs.symlinkSync("../../public/assets", pathToAssetsLn);
