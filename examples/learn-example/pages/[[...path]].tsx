77da0d5693f62fa331e5f1c9b00e3de920dd26a2
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-05-03 10:17:12 -0300

    [learn-example][sm] - dont panic when no markdown.db file found

--- examples/learn-example/pages/[[...path]].tsx
+++ examples/learn-example/pages/[[...path]].tsx
@@ -1,4 +1,4 @@
-import { promises as fs } from 'fs';
+import { existsSync, promises as fs } from 'fs';
 import path from 'path';
 import parse from '../lib/markdown';
 import DRD from '../components/DRD';
@@ -24,18 +24,22 @@ export const getStaticProps = async (context) => {
     pathToFile = context.params.path.join('/') + '/index.md';
   }
 
-  const mddb = await clientPromise;
-  const datasetsFiles = await mddb.getFiles({
-    extensions: ['md', 'mdx'],
-  });
-  const datasets = datasetsFiles
-    .filter((dataset) => dataset.url_path !== '/')
-    .map((dataset) => ({
-      _id: dataset._id,
-      url_path: dataset.url_path,
-      file_path: dataset.file_path,
-      metadata: dataset.metadata,
-    }));
+  let datasets = [];
+  const mddbFileExists = existsSync('markdown.db');
+  if (mddbFileExists) {
+    const mddb = await clientPromise;
+    const datasetsFiles = await mddb.getFiles({
+      extensions: ['md', 'mdx'],
+    });
+    datasets = datasetsFiles
+      .filter((dataset) => dataset.url_path !== '/')
+      .map((dataset) => ({
+        _id: dataset._id,
+        url_path: dataset.url_path,
+        file_path: dataset.file_path,
+        metadata: dataset.metadata,
+      }));
+  }
 
   const indexFile = path.join(process.cwd(), '/content/' + pathToFile);
   const readme = await fs.readFile(indexFile, 'utf8');
