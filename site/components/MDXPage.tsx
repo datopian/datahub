5c8431bf394a12ab6f7be08eb58c737ab8211ec5
João Demenech <joao.demenech@datopian.com>
2023-05-06 13:36:49 -0300

    Fix code blocks not being displayed properly on light mode (#851)
    * [#803,blogs][m]: fix code blocks not being displayed properly on light mode
    
    * [docs][m] - fix problems with merge
    
    ---------
    
    Co-authored-by: Luccas Mateus <Luccasmmg@gmail.com>

--- site/components/MDXPage.tsx
+++ site/components/MDXPage.tsx
@@ -4,38 +4,15 @@ import DocsPagination from './DocsPagination';
 
 export default function MDXPage({ source, frontMatter }) {
   const Layout = ({ children }) => {
-    if (frontMatter.layout) {
-      let LayoutComponent = layouts[frontMatter.layout];
-      return <LayoutComponent {...frontMatter}>{children}</LayoutComponent>;
-    }
-    return <>{children}</>;
+    const layoutName = frontMatter?.layout || 'default';
+    const LayoutComponent = layouts[layoutName];
+
+    return <LayoutComponent {...frontMatter}>{children}</LayoutComponent>;
   };
 
   return (
-    <div className="prose mx-auto prose-a:text-primary dark:prose-a:text-primary-dark prose-strong:text-primary dark:prose-strong:text-primary-dark prose-headings:text-primary dark:prose-headings:text-primary-dark text-primary dark:text-primary-dark prose-headings:font-headings dark:prose-invert prose-a:break-words">
-      <header>
-        <div className="mb-6">
-          {/* Default layout */}
-          {!frontMatter.layout && (
-            <>
-              <h1>{frontMatter.title}</h1>
-              {frontMatter.author && (
-                <div className="-mt-6">
-                  <p className="opacity-60 pl-1">{frontMatter.author}</p>
-                </div>
-              )}
-              {frontMatter.description && (
-                <p className="description">{frontMatter.description}</p>
-              )}
-            </>
-          )}
-        </div>
-      </header>
-      <main>
-        <Layout>
-          <MDXRemote {...source} components={{ DocsPagination }} />
-        </Layout>
-      </main>
-    </div>
+    <Layout>
+      <MDXRemote {...source} components={{ DocsPagination }} />
+    </Layout>
   );
 }
--- site/layouts/index.ts
+++ site/layouts/index.ts
@@ -1,13 +1,15 @@
 import {
   SimpleLayout,
-  DocsLayout,
   UnstyledLayout,
-  BlogLayout,
 } from "@flowershow/core";
+import { BlogLayout } from "./blog";
+import DefaultLayout from "./default";
+import { DocsLayout } from "./docs";
 
 export default {
   simple: SimpleLayout,
   docs: DocsLayout,
   unstyled: UnstyledLayout,
   blog: BlogLayout,
+  default: DefaultLayout
 };
