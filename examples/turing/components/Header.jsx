7c943c1b319271e00b74aba656026ec251416279
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-05-11 17:16:56 -0300

    [example/turing][sm] - forgot to add github on desktop

--- examples/turing/components/Header.jsx
+++ examples/turing/components/Header.jsx
@@ -179,7 +179,7 @@ function DesktopNavigation(props) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="https://github.com/leondz/hatespeechdata">
+        <NavItem href="https://github.com/datopian/portaljs/tree/main/examples/turing">
           View on Github <GithubIcon />
         </NavItem>
       </ul>
