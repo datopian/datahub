8327f4efc04a9bb7c95e7744f3ed913f7b5cc353
João Demenech <joaommdemenech@gmail.com>
2023-08-07 15:15:43 -0300

    [site,storybook,seo][xs]: fix duplicate user canonical issues -- refs #957

--- site/next-sitemap.config.js
+++ site/next-sitemap.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://portaljs.org',
   generateRobotsTxt: true,
-}
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', disallow: '/people' },
+      { userAgent: '*', disallow: '/?amp=1' },
+    ],
+  },
+};
